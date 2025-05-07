52dacf135b66cdd4d0f387716a8b141e4c64d1a6
irbansin <ani.expo10@gmail.com>
2025-05-07 17:11:31 +0530

    set up prisma connection with local db

---
+++ prisma/migrations/20250507112928_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Image" (
+    "id" TEXT NOT NULL,
+    "filename" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Image_pkey" PRIMARY KEY ("id")
+);
